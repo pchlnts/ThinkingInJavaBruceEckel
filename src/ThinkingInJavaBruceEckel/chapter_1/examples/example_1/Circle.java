f28e47c5135e5039077ea5739a80e117fc69a045
pchlnts <47396694+pchlnts@users.noreply.github.com>
2020-07-10 16:44:47 +0300

    A simple example of polymorphism from the first chapter.

---
+++ src/ThinkingInJavaBruceEckel/chapter_1/examples/example_1/Circle.java
@@ -0,0 +1,11 @@
+package ThinkingInJavaBruceEckel.chapter_1.examples.example_1;
+
+public class Circle extends Shape {
+    public void erase() {
+        System.out.println("Erasing the Circle");
+    }
+
+    public void draw() {
+        System.out.println("Circle drawing");
+    }
+    }
